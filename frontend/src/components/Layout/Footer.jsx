f6eb288a1810479d3860f6925a7cb30483246105
Vatsalmk28 <vatsalmk28@gmail.com>
2025-07-07 22:02:32 +0530

    Updated Socials

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -8,12 +8,10 @@ function Footer() {
   const {isAuthorized}  = useContext(Context)
   return (
     <footer className= {isAuthorized ? "footerShow" : "footerHide"}>
-<div>&copy; All Rights Reserved by Abhishek.</div>
+<div>&copy; All Rights Reserved by VamK_28.</div>
 <div>
-  <Link to={'https://github.com/exclusiveabhi'} target='github'><FaGithub></FaGithub></Link>
-  <Link to={'https://leetcode.com/u/exclusiveabhi/'} target='leetcode'><SiLeetcode></SiLeetcode></Link>
-  <Link to={'https://www.linkedin.com/in/abhishek-rajput-/'} target='linkedin'><FaLinkedin></FaLinkedin></Link>
-  <Link to={'https://www.instagram.com/exclusiveabhi/'} target='instagram'><RiInstagramFill></RiInstagramFill></Link>
+  <Link to={'https://github.com/Vatsalmk28'} target='github'><FaGithub></FaGithub></Link>
+  <Link to={'https://www.linkedin.com/in/vatsal-kukadiya/'} target='linkedin'><FaLinkedin></FaLinkedin></Link>
 </div>
       
     </footer>
